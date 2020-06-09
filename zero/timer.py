f0353cfefd6f2fc1427e261633b9af6ce62a5a66
Yury Gorishniy <strausmg@gmail.com>
2020-06-09 18:54:40 +0300

    rename _pause_start_time to _stop_time

--- zero/timer.py
+++ zero/timer.py
@@ -6,7 +6,7 @@ from typing import Optional, Union
 class Timer:
     # mypy cannot infer types from .reset(), so they must be given here
     _start_time: Optional[float]
-    _pause_start_time: Optional[float]
+    _stop_time: Optional[float]
     _shift: float
 
     def __init__(self) -> None:
@@ -15,18 +15,18 @@ class Timer:
     def start(self) -> 'Timer':
         if self._start_time is None:
             self._start_time = time.perf_counter()
-        elif self._pause_start_time is not None:
-            self.sub(time.perf_counter() - self._pause_start_time)
-            self._pause_start_time = None
+        elif self._stop_time is not None:
+            self.sub(time.perf_counter() - self._stop_time)
+            self._stop_time = None
         return self  # enables the pattern `timer = Timer().start()`
 
     def stop(self) -> None:
-        if self._pause_start_time is None:
-            self._pause_start_time = time.perf_counter()
+        if self._stop_time is None:
+            self._stop_time = time.perf_counter()
 
     def reset(self) -> None:
         self._start_time = None
-        self._pause_start_time = None
+        self._stop_time = None
         self._shift = 0.0
 
     def add(self, shift: float) -> None:
@@ -38,6 +38,6 @@ class Timer:
     def __call__(self, round_up: bool = False) -> Union[float, int]:
         if self._start_time is None:
             return 0 if round_up else 0.0
-        now = self._pause_start_time or time.perf_counter()
+        now = self._stop_time or time.perf_counter()
         result = now - self._start_time + self._shift
         return math.ceil(result) if round_up else result
