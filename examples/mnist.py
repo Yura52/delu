18237d843f0290e82487a24d22af4d17d22f8a4a
Yury Gorishniy <strausmg@gmail.com>
2020-10-10 01:07:48 +0300

    update example

--- examples/mnist.py
+++ examples/mnist.py
@@ -15,20 +15,7 @@ except ImportError:
     )
     raise
 
-from zero.all import (
-    Eval,
-    Stream,
-    ProgressTracker,
-    Timer,
-    concat,
-    dump_json,
-    format_seconds,
-    free_memory,
-    get_gpu_info,
-    learn,
-    set_randomness,
-    to_device,
-)
+import zero
 
 
 def get_dataset(train):
@@ -47,7 +34,7 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument('-d', '--device', default='cpu', type=torch.device)
     parser.add_argument('-e', '--epoch-size')
-    parser.add_argument('-n', '--n-epoches', type=int, default=999)
+    parser.add_argument('-n', '--n-epoches', type=int, default=20)
     parser.add_argument('-p', '--early-stopping-patience', type=int, default=2)
     parser.add_argument('-s', '--seed', type=int)
     return parser.parse_args()
@@ -56,28 +43,28 @@ def parse_args():
 def main():
     args = parse_args()
 
-    set_randomness(args.seed)
+    zero.set_randomness(args.seed)
     model = nn.Linear(784, 10).to(args.device)
     optimizer = torch.optim.SGD(model.parameters(), 0.005, 0.9)
 
     def step(batch):
-        X, y = to_device(batch, args.device)
-        return model(X), y
+        X, y = batch
+        return model(X.to(args.device)), y.to(args.device)
 
-    def calculate_accuracy(loader):
-        with Eval(model):
-            logits, y = concat(map(step, loader))
+    def evaluate(loader):
+        with zero.Eval(model):
+            logits, y = zero.concat(map(step, loader))
         y_pred = torch.argmax(logits, dim=1).to(y)
         return (y_pred == y).int().sum().item() / len(y)
 
     train_dataset, val_dataset = split_dataset(get_dataset(True), 0.8)
     test_dataset = get_dataset(False)
-    stream = Stream(DataLoader(train_dataset, batch_size=64, shuffle=True))
+    stream = zero.Stream(DataLoader(train_dataset, batch_size=64, shuffle=True))
     val_loader = DataLoader(val_dataset, batch_size=8096)
     test_loader = DataLoader(test_dataset, batch_size=8096)
 
-    timer = Timer()
-    progress = ProgressTracker(args.early_stopping_patience, 0.005)
+    timer = zero.Timer()
+    progress = zero.ProgressTracker(args.early_stopping_patience, 0.005)
     best_model_path = 'model.pt'
 
     for epoch in stream.epoches(args.n_epoches, args.epoch_size):
@@ -85,43 +72,37 @@ def main():
         timer.run()
 
         for batch in epoch:
-            learn(model, optimizer, F.cross_entropy, step, batch, True)
+            zero.learn(model, optimizer, F.cross_entropy, step, batch, True)
 
         timer.pause()
-        accuracy = calculate_accuracy(val_loader)
+        accuracy = evaluate(val_loader)
         progress.update(accuracy)
         if progress.success:
             torch.save(model.state_dict(), best_model_path)
 
         msg = (
             f'Epoch {stream.epoch} finished. '
-            f'Time elapsed: {format_seconds(timer())}. '
+            f'Time elapsed: {zero.format_seconds(timer())}. '
             f'Validation accuracy: {accuracy:.4f}.'
         )
         if args.device.type == 'cuda':
             index = args.device.index or 0
-            msg += f'\nGPU info: {get_gpu_info()[index]}'
+            msg += f'\nGPU info: {zero.get_gpu_info()[index]}'
         print(msg)
         if progress.fail:
             break
 
     timer.pause()
+    model.load_state_dict(torch.load(best_model_path))
     print(
-        f'\nTraining stopped after the epoch {stream.epoch}. '
-        f'Total training time: {format_seconds(timer())}'
+        f'\nTraining stopped after the epoch {stream.epoch}.\n'
+        f'Total training time: {zero.format_seconds(timer())}\n'
+        f'Test accuracy: {evaluate(test_loader)}'
     )
 
-    model.load_state_dict(torch.load(best_model_path))
-    report = {'accuracy': calculate_accuracy(test_loader)}
-    report['training_time'] = timer()
-    report_path = 'report.json'
-    dump_json(report, report_path, indent=4)
-    print(f'The experiment report is saved to {report_path}')
-
     print('Freeing memory (for fun, not for profit) ...')
-    del model, optimizer, step
-    free_memory()
-
+    del model, optimizer, step, evaluate
+    zero.free_memory()
     print('\nDONE.')
 
 
