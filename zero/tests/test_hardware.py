07760e09480099f08e47a129a85eb6385630bfe7
Yury Gorishniy <strausmg@gmail.com>
2021-04-11 19:33:37 +0300

    add tests for get_gpus_info

--- zero/tests/test_hardware.py
+++ zero/tests/test_hardware.py
@@ -1,6 +1,8 @@
 import gc
+from collections import namedtuple
 from unittest.mock import patch
 
+import pynvml
 import torch
 from pynvml import NVMLError_LibraryNotFound
 from pytest import mark, raises
@@ -19,6 +21,53 @@ def test_free_memory(gpu):
         if gpu:
             torch.cuda.synchronize.assert_called_once()
             torch.cuda.empty_cache.assert_called_once()
+
+
+def test_get_gpus_info():
+    Memory = namedtuple('Memory', ['total', 'free', 'used'])
+    Utilization = namedtuple('Utilization', ['gpu'])
+    memory = Memory(3, 2, 1)
+    utilization = Utilization(10)
+    driver = '123.456'
+    name = 'ABC123'
+    n_devices = 2
+    pynvml_fns = [
+        f'pynvml.{x}'
+        for x in [
+            'nvmlInit',
+            'nvmlDeviceGetCount',
+            'nvmlDeviceGetHandleByIndex',
+            'nvmlDeviceGetMemoryInfo',
+            'nvmlDeviceGetName',
+            'nvmlDeviceGetUtilizationRates',
+            'nvmlSystemGetDriverVersion',
+        ]
+    ]
+
+    with patch(pynvml_fns[0]), patch(pynvml_fns[1], return_value=n_devices), patch(
+        pynvml_fns[2], return_value=0
+    ), patch(pynvml_fns[3], return_value=memory), patch(
+        pynvml_fns[4], return_value=name.encode('utf-8')
+    ), patch(
+        pynvml_fns[5], return_value=utilization
+    ), patch(
+        pynvml_fns[6], return_value=driver.encode('utf-8')
+    ):
+        assert hardware.get_gpus_info() == {
+            'driver': driver,
+            'devices': [
+                {
+                    'name': name,
+                    'memory_total': memory.total,
+                    'memory_free': memory.free,
+                    'memory_used': memory.used,
+                    'utilization': utilization.gpu,
+                }
+            ]
+            * n_devices,
+        }
+        for fn in pynvml_fns:
+            getattr(pynvml, fn.split('.')[1]).assert_called()
 
 
 def test_get_gpus_info_without_gpu():
