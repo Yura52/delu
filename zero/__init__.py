78192159c540b734fc8b6539600171f2e1e9e032
Yury Gorishniy <strausmg@gmail.com>
2021-05-08 19:25:17 +0300

    remove extra files

--- zero/__init__.py
+++
@@ -1,9 +0,0 @@
-"""A zero-overhead library for PyTorch users."""
-__version__ = '0.0.3.dev10'
-
-from . import data  # noqa
-from . import hardware  # noqa
-from . import random  # noqa
-from .data import collate, concat, iter_batches  # noqa
-from .stream import Stream  # noqa
-from .utils import ProgressTracker, Timer, evaluation  # noqa
