e6ee874ced3da735b1b139a0d2b212061500c042
Yura52 <strausmg@gmail.com>
2022-06-05 02:13:27 +0300

    version | 0.0.10

--- delu/__init__.py
+++ delu/__init__.py
@@ -1,5 +1,5 @@
-"""Deep Learning Utilities."""
-__version__ = '0.0.11.dev0'
+"""Deep Learning Utilities for PyTorch users."""
+__version__ = '0.0.10'
 
 from . import data  # noqa
 from . import hardware  # noqa
