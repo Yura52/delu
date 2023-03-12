8de45165a4d764f016c930513001c86d59acf8fa
Yura Gorishniy <strausmg@gmail.com>
2023-03-12 21:34:28 +0300

    delu.ProgressTracker | improve docs

--- delu/_monitoring.py
+++ delu/_monitoring.py
@@ -11,7 +11,7 @@ class _ProgressStatus(enum.Enum):
 
 
 class ProgressTracker:
-    """Facilitates early stopping and tracks the best metric value.
+    """Helps with early stopping and tracks the best metric value.
 
     For `~ProgressTracker`, **the greater score is the better score**.
     At any moment the tracker is in one of the following states:
