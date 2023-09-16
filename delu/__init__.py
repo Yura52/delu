c5e1583249d5d253eaec7254dec2fcc53ed2f1f3
Yura52 <strausmg@gmail.com>
2023-09-16 23:04:45 +0300

    delu | define __all__

--- delu/__init__.py
+++ delu/__init__.py
@@ -1,13 +1,31 @@
 """Deep Learning Utilities for PyTorch users."""
 __version__ = '0.0.19.dev0'
 
-from . import cuda  # noqa: F401
-from . import data  # noqa: F401
-from . import hardware  # noqa: F401
-from . import nn  # noqa: F401
-from . import random  # noqa: F401
-from ._stream import Stream  # noqa: F401
-from ._tensor_ops import cat, concat, iter_batches, to  # noqa: F401
-from ._tools import EarlyStopping, ProgressTracker, Timer  # noqa: F401
-from ._utilities import evaluation, improve_reproducibility  # noqa: F401
-from .data import collate  # noqa: F401
+from . import cuda, data, hardware, nn, random
+from ._stream import Stream
+from ._tensor_ops import cat, concat, iter_batches, to
+from ._tools import EarlyStopping, ProgressTracker, Timer
+from ._utilities import evaluation, improve_reproducibility
+from .data import collate
+
+__all__ = [
+    'cuda',
+    'data',
+    'nn',
+    'random',
+    #
+    'EarlyStopping',
+    'Timer',
+    'cat',
+    'iter_batches',
+    'to',
+    # DEPRECATED.
+    'hardware',
+    #
+    'collate',
+    'concat',
+    'evaluation',
+    'improve_reproducibility',
+    'ProgressTracker',
+    'Stream',
+]
