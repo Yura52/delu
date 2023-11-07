5f0015cbdff86f64aff8199123012a9663538fcf
Yura52 <strausmg@gmail.com>
2023-11-08 01:47:42 +0300

    version | 0.0.24.dev0

--- delu/__init__.py
+++ delu/__init__.py
@@ -1,5 +1,5 @@
 """Deep Learning Utilities for PyTorch users."""
-__version__ = '0.0.23'
+__version__ = '0.0.24.dev0'
 
 from . import cuda, data, hardware, nn, random, tools, utils
 from ._stream import Stream
