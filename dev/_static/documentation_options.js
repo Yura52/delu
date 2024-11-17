3421591346dfb1cf041fca283e8539ad4673c851
Yury Gorishniy <strausmg@gmail.com>
2024-11-17 14:13:12 +0300

    update dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.26.dev0',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.0.27.dev0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
